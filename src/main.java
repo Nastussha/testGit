5f6bf573f02be3e992b626c4bad34b9d387e3c9a
Nastussha <v-n-09@yandex.ru>
2019-11-14 08:15:31 +0300

    1 commit

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("testik99999");
+        System.out.println("testik99999");
+        System.out.println("testik99999");
 
     }
 }
