1c2468d761627ac88b458614738e41d65f854ed1
Nastussha <v-n-09@yandex.ru>
2019-11-14 08:26:43 +0300

    update commit

--- src/main.java
+++ src/main.java
@@ -1,10 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("testik99999");
-        System.out.println("testik99999");
-        System.out.println("testik99999");
-        System.out.println("testik99999");
-        System.out.println("testik99999");System.out.println("testik99999");System.out.println("testik99999");
 
 
     }
